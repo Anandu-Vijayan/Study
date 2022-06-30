6ceab8230b1ec3d213a27bd96c50771c9bae52f9
anandu <ananduvijayan8@gmail.com>
2022-06-30 14:18:10 +0530

    Admin created

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,39 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  let product=[
+    {
+      Name:"Becardi",
+      Discription:"Nice Drink",
+      Price:5000,
+      Image:"https://cdn2.bigcommerce.com/server5500/tpbc2s65/products/420/images/6272/183061.primary_images__09938.1497900092.1280.1280.jpg?c=2"
+
+    },
+    {
+      Name:"Becardi",
+      Discription:"Nice Drink",
+      Price:5000,
+      Image:"https://cdn2.bigcommerce.com/server5500/tpbc2s65/products/420/images/6272/183061.primary_images__09938.1497900092.1280.1280.jpg?c=2"
+
+    },
+    {
+      Name:"Becardi",
+      Discription:"Nice Drink",
+      Price:5000,
+      Image:"https://cdn2.bigcommerce.com/server5500/tpbc2s65/products/420/images/6272/183061.primary_images__09938.1497900092.1280.1280.jpg?c=2"
+
+    },
+    {
+      Name:"Becardi",
+      Discription:"Nice Drink",
+      Price:5000,
+      Image:"https://cdn2.bigcommerce.com/server5500/tpbc2s65/products/420/images/6272/183061.primary_images__09938.1497900092.1280.1280.jpg?c=2"
+
+    },
+    
+
+  ]
+  res.render('admin/view-product',{admin:true,product})
 });
 
 module.exports = router;
