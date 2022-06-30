bd1e9a647635591bda8ef3d2d93050afe9d521cb
anandu <ananduvijayan8@gmail.com>
2022-06-30 13:32:41 +0530

    first commit

---
+++ routes/user.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  let product=[
+    {
+      Name:"Becardi",
+      Discription:"Nice Drink",
+      Price:5000,
+      Image:"https://cdn2.bigcommerce.com/server5500/tpbc2s65/products/420/images/6272/183061.primary_images__09938.1497900092.1280.1280.jpg?c=2"
+
+    },
+    {
+      Name:"Becardi",
+      Discription:"Nice Drink",
+      Price:5000,
+      Image:"https://cdn2.bigcommerce.com/server5500/tpbc2s65/products/420/images/6272/183061.primary_images__09938.1497900092.1280.1280.jpg?c=2"
+
+    },
+    {
+      Name:"Becardi",
+      Discription:"Nice Drink",
+      Price:5000,
+      Image:"https://cdn2.bigcommerce.com/server5500/tpbc2s65/products/420/images/6272/183061.primary_images__09938.1497900092.1280.1280.jpg?c=2"
+
+    },
+    {
+      Name:"Becardi",
+      Discription:"Nice Drink",
+      Price:5000,
+      Image:"https://cdn2.bigcommerce.com/server5500/tpbc2s65/products/420/images/6272/183061.primary_images__09938.1497900092.1280.1280.jpg?c=2"
+
+    },
+    
+
+  ]
+  res.render('index', {product});
+});
+
+module.exports = router;
